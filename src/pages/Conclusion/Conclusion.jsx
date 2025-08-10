88b70bfe2b3a448cba21e21e7c9d753570c83c11
guilherme andre <guilhermeanm95@hotmail.com>
2025-08-10 01:59:02 -0300

    Criação de componentes

---
+++ src/pages/Conclusion/Conclusion.jsx
@@ -0,0 +1,10 @@
+function Conclusion() {
+  return (
+    <div>
+      <h1>Bem-vindo à Home</h1>
+      <p>Essa é a página inicial do nosso site React 19.</p>
+    </div>
+  );
+}
+
+export default Conclusion;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo à Home</h1>
+      <p>Essa é a página inicial do nosso site React 19.</p>
+    </div>
+  );
+}
+
+export default Home;
